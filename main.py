0130035fc6f26605b426b320c65f8103e6aeaaa6
elifbabaoglu <62393418+elifbabaoglu@users.noreply.github.com>
2020-03-19 13:25:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 def has_one_one_mapping(s1, s2):
     if len(s1) <= len(s2):
         d = {}
@@ -8,3 +10,29 @@ def has_one_one_mapping(s1, s2):
                 return False
         return True
     return False
+
+def len_different_one_one_mapping(s1, s2):
+    set1 = set()
+    set2 = set()
+    for c in s1:
+        set1.add(c)
+    for c in s2:
+        set2.add(c)
+    if len(set1) >= len(set2):
+        return True
+    else:
+        return False
+
+def main():
+    if len(sys.argv) != 3:
+        return False
+    s1 = sys.argv[1]
+    s2 = sys.argv[2]
+    str1_length = len(s1)
+    str2_length = len(s2)
+    if str1_length == str2_length:
+        print(has_one_one_mapping(s1, s2))
+    else:
+        print(len_different_one_one_mapping(s1, s2))
+
+main()
